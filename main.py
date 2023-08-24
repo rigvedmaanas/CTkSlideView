e0e92709c033e2bfdd8b55d79f44d4aa008d142a
Rigved Maanas <77579661+rigvedmaanas@users.noreply.github.com>
2023-08-24 21:52:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ set_default_color_theme("dark-blue")
 
 root = CTk()
 root.geometry("1800x900")
-root.title("CTk SlideView")
+root.title("CTk SlideMenu")
 
-
-#An Example For Slide Menu
 
 slide_menu = CTkSlideView(root)
 slide_menu.place(relx=0.5, rely=0.5, anchor=CENTER)
-lbl = CTkLabel(slide_menu.create_tab(), text="Slide 1", font=("SF Display", 50)).place(relx=0.5, rely=0.5, anchor=CENTER)
-lbl2 = CTkLabel(slide_menu.create_tab(), text="Slide 2", font=("SF Display", 50)).place(relx=0.5, rely=0.5, anchor=CENTER)
-lbl3 = CTkLabel(slide_menu.create_tab(), text="Slide 3", font=("SF Display", 50)).place(relx=0.5, rely=0.5, anchor=CENTER)
-lbl4 = CTkLabel(slide_menu.create_tab(), text="Slide 4", font=("SF Display", 50)).place(relx=0.5, rely=0.5, anchor=CENTER)
-lbl5 = CTkLabel(slide_menu.create_tab(), text="Slide 5", font=("SF Display", 50)).place(relx=0.5, rely=0.5, anchor=CENTER)
 
 
+# slide_menu.create_tab() will return a frame where you can display anything.
 
+# Some Example Slides
+CTkLabel(slide_menu.create_tab(), text="Slide 1", font=("SF Display", 50)).place(relx=0.5, rely=0.5, anchor=CENTER)
 
+CTkLabel(slide_menu.create_tab(), text="Slide 2", font=("SF Display", 50)).place(relx=0.5, rely=0.5, anchor=CENTER)
+
+CTkLabel(slide_menu.create_tab(), text="Slide 3", font=("SF Display", 50)).place(relx=0.5, rely=0.5, anchor=CENTER)
+
+CTkLabel(slide_menu.create_tab(), text="Slide 4", font=("SF Display", 50)).place(relx=0.5, rely=0.5, anchor=CENTER)
+
+CTkLabel(slide_menu.create_tab(), text="Slide 5", font=("SF Display", 50)).place(relx=0.5, rely=0.5, anchor=CENTER)
 
 root.mainloop()
